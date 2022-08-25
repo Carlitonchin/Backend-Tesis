a49d20ddf148e423ad3141da4b875d2b280d550c
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-25 11:57:57 -0300

    context parameter added to a UserService and UserRepository interfaces

--- model/interfaces.go
+++ model/interfaces.go
@@ -1,9 +1,11 @@
 package model
 
+import "context"
+
 type UserService interface {
-	GetById(id int) (*User, error)
+	GetById(ctx context.Context, id int) (*User, error)
 }
 
 type UserRepository interface {
-	FindById(id int) (*User, error)
+	FindById(ctx context.Context, id int) (*User, error)
 }
