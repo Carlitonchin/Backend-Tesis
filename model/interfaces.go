b46f716216aff678cf7b227ed65a62fc4f8fe7b3
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-25 16:15:36 -0300

    SignUp method added to UserService interface

--- model/interfaces.go
+++ model/interfaces.go
@@ -4,6 +4,7 @@ import "context"
 
 type UserService interface {
 	GetById(ctx context.Context, id uint) (*User, error)
+	SignUp(ctx context.Context, user *User) error
 }
 
 type UserRepository interface {
