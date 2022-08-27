a04977318bea3b901149680a9f6dd28596ff8c48
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-27 18:06:37 -0300

    compare suplied pass and store pass function added

--- service/password.go
+++ service/password.go
@@ -4,11 +4,12 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math/rand"
+	"strings"
 
 	"golang.org/x/crypto/scrypt"
 )
 
-func hash_options() (int, int, int, int) {
+func scrypt_options() (int, int, int, int) {
 	N := 32768
 	r := 8
 	p := 1
@@ -25,7 +26,7 @@ func hashPass(pass string) (string, error) {
 		return "", err
 	}
 
-	N, r, p, key_length := hash_options()
+	N, r, p, key_length := scrypt_options()
 
 	shash, err := scrypt.Key([]byte(pass), salt, N, r, p, key_length)
 
@@ -36,4 +37,23 @@ func hashPass(pass string) (string, error) {
 	hassed_pass := fmt.Sprintf("%s.%s", hex.EncodeToString(shash), hex.EncodeToString(salt))
 
 	return hassed_pass, nil
+}
+
+func comparePass(suplied string, stored string) (bool, error) {
+	pass_salt := strings.Split(stored, ".")
+
+	salt, err := hex.DecodeString(pass_salt[1])
+
+	if err != nil {
+		return false, err
+	}
+
+	N, r, p, key_length := scrypt_options()
+	suplied_hash, err := scrypt.Key([]byte(suplied), salt, N, r, p, key_length)
+
+	if err != nil {
+		return false, err
+	}
+
+	return hex.EncodeToString(suplied_hash) == pass_salt[0], nil
 }
