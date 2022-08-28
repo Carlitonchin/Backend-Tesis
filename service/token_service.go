f672eb7df4b0cab13acf487b3e97c42565a13d65
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-28 16:19:12 -0300

    mock implementation for interface model.TokenService by service.TokenService

--- service/token_service.go
+++ service/token_service.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"context"
 	"crypto/rsa"
+
+	"github.com/Carlitonchin/Backend-Tesis/model"
 )
 
 type TokenService struct {
@@ -14,4 +17,21 @@ type TSConfig struct {
 	PrivateKey    *rsa.PrivateKey
 	PublicKey     *rsa.PublicKey
 	RefreshSecret string
+}
+
+func NewTokenService(c *TSConfig) model.TokenService {
+	return &TokenService{
+		PrivateKey:    c.PrivateKey,
+		PublicKey:     c.PublicKey,
+		RefreshSecret: c.RefreshSecret,
+	}
+}
+
+func (s *TokenService) GetNewPairFromUser(
+	ctx context.Context,
+	user *model.User,
+	prevTokenId string) (model.TokenPair, error) {
+
+	//function body starts here
+	panic("Not implemented yet")
 }
