c615b3757f8e2208d9f45fb4d5b7d840f5ea619a
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-29 23:28:17 -0300

    change user repository struct visibility to private

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -7,23 +7,23 @@ import (
 	"gorm.io/gorm"
 )
 
-type UserRepository struct {
+type userRepository struct {
 	DB *gorm.DB
 }
 
 func NewUserRepository(db *gorm.DB) model.UserRepository {
-	return &UserRepository{
+	return &userRepository{
 		DB: db,
 	}
 }
 
-func (s *UserRepository) Create(ctx context.Context, user *model.User) error {
+func (s *userRepository) Create(ctx context.Context, user *model.User) error {
 	result := s.DB.Create(user)
 
 	return result.Error
 }
 
-func (s *UserRepository) FindById(ctx context.Context, id uint) (*model.User, error) {
+func (s *userRepository) FindById(ctx context.Context, id uint) (*model.User, error) {
 	var user model.User
 
 	result := s.DB.First(&user, id)
