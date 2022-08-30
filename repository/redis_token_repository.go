14bc886fafd25e478b574918f5e8dece5180fdf4
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-30 16:05:47 -0300

    implemented TokenRepository functions

--- repository/redis_token_repository.go
+++ repository/redis_token_repository.go
@@ -1,6 +1,10 @@
 package repository
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"github.com/Carlitonchin/Backend-Tesis/model"
 	"github.com/go-redis/redis/v8"
 )
@@ -13,4 +17,22 @@ func NewTokenRepository(redis_client *redis.Client) model.TokenRepository {
 	return &redisTokenRepository{
 		RedisClient: redis_client,
 	}
+}
+
+func (s *redisTokenRepository) SetNewRefreshToken(ctx context.Context, userId string,
+	tokenId string, expiresIn time.Duration) error {
+
+	key := fmt.Sprintf("%s:%s", userId, tokenId)
+	err := s.RedisClient.Set(ctx, key, 0, expiresIn).Err()
+
+	return err
+}
+
+func (s *redisTokenRepository) DeleteRefreshToken(ctx context.Context,
+	userId string, prevTokenId string) error {
+
+	key := fmt.Sprintf("%s:%s", userId, prevTokenId)
+	err := s.RedisClient.Del(ctx, key).Err()
+
+	return err
 }
