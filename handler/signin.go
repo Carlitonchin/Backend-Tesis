38df04dfbdeb23341accdc5c8c37fc5b0d5081d4
Carlitonchin <carlozalejandro98@gmail.com>
2022-08-31 14:06:06 -0300

    signin handler implemented

--- handler/signin.go
+++ handler/signin.go
@@ -1,7 +1,53 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Carlitonchin/Backend-Tesis/model"
+	"github.com/Carlitonchin/Backend-Tesis/model/apperrors"
+	"github.com/gin-gonic/gin"
+)
+
+type siginReq struct {
+	Email string `json:"email" binding:"required,email"`
+	Pass  string `json:"pass" binding:"required,gte=6,lte=30"`
+}
 
 func (h *Handler) signin(ctx *gin.Context) {
-	panic("not implemented")
+	var req siginReq
+
+	if ok := bindData(ctx, &req); !ok {
+		return // response returned inside of bindData function
+	}
+
+	u := &model.User{
+		Email:    req.Email,
+		Password: req.Pass,
+	}
+
+	err := h.UserService.SignIn(ctx.Request.Context(), u)
+
+	if err != nil {
+		ctx.JSON(apperrors.Status(err), gin.H{
+			"error": err,
+		})
+
+		return
+	}
+
+	// user singin succesfully
+	tokens, err := h.TokenService.GetNewPairFromUser(ctx.Request.Context(), u, "")
+
+	if err != nil {
+		ctx.JSON(apperrors.Status(err), gin.H{
+			"error": err,
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"tokens": tokens,
+	})
+
 }
