8fcc966bf8bda6b944dd300dfa1bee3953012597
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-01 16:46:31 -0300

    add level for user instead of role

--- model/role.go
+++ model/role.go
@@ -5,6 +5,5 @@ import "gorm.io/gorm"
 type Role struct {
 	gorm.Model
 	Name  string `json:"name" gorm:"unique;not null"`
-	Level int    `json:"level"`
 	Users []User `json:"-" gorm:"foreignKey:RoleID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
--- model/user.go
+++ model/user.go
@@ -8,5 +8,6 @@ type User struct {
 	Name     string `json:"name" gorm:"unique;not null"`
 	Password string `json:"-" gorm:"not null"`
 	Worker   bool   `json:"worker" gorm:"not null"`
+	Level    int    `json:"level"`
 	RoleID   uint   `json:"role"`
 }
