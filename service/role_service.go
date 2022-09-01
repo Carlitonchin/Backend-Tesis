03fe6e1799febe364b3639464836218406e50851
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-01 18:48:28 -0300

    add getroleByName implementation for role service

--- service/role_service.go
+++ service/role_service.go
@@ -19,3 +19,7 @@ func NewRoleService(repo model.RoleRepository) model.RoleService {
 func (s *roleService) GetRoles(ctx context.Context) ([]model.Role, error) {
 	return s.RoleRepository.GetRoles(ctx)
 }
+
+func (s *roleService) GetRoleByName(ctx context.Context, role_name string) (*model.Role, error) {
+	return s.RoleRepository.GetRoleByName(ctx, role_name)
+}
