376ccc52d40a4b1eda18e82e9de11b8268130189
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-02 22:42:56 -0300

    add status question relationship

--- model/question.go
+++ model/question.go
@@ -4,6 +4,8 @@ import "gorm.io/gorm"
 
 type Question struct {
 	gorm.Model
-	Text      string `json:"text" gorm:"not null"`
-	UserRefer uint   `json:"-"`
+	Text      string  `json:"text" gorm:"not null"`
+	UserRefer uint    `json:"user_id"`
+	StatusId  uint    `json:"status_id"`
+	Status    *Status `json:"status" gorm:"-"`
 }
--- model/status.go
+++ model/status.go
@@ -4,5 +4,6 @@ import "gorm.io/gorm"
 
 type Status struct {
 	gorm.Model
-	Description string `json:"description" gorm:"unique;not null"`
+	Description string     `json:"description" gorm:"unique;not null"`
+	Questions   []Question `json:"-" gorm:"foreignKey:StatusId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
