44f35200ca37ad5215e9950e9aa461519d8cb466
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-02 22:20:56 -0300

    add seed status

--- seed.go
+++ seed.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/Carlitonchin/Backend-Tesis/model"
 	"github.com/Carlitonchin/Backend-Tesis/service"
@@ -17,6 +18,7 @@ func seed(db *gorm.DB) {
 	if err != nil {
 		seedRoles(db)
 		seedUsers(db)
+		seedStatus(db)
 	}
 }
 
@@ -28,7 +30,7 @@ func seedRoles(db *gorm.DB) {
 	errc := db.Create(&model.Role{Name: os.Getenv("ROLE_DEFAULT_WORKER")}).Error
 	erre := db.Create(&model.Role{Name: os.Getenv("ROLE_DEFAULT_STUDENT")}).Error
 
-	if erra != nil || errc != nil || erre != nil {
+	if atLeastOneError(erra, errc, erre) {
 		log.Fatal("Error at seed data for roles")
 	}
 }
@@ -48,8 +50,55 @@ func seedUsers(db *gorm.DB) {
 		RoleID:   role.ID,
 	}).Error
 
-	if errf != nil || errp != nil || erru != nil {
+	if atLeastOneError(errf, errp, erru) {
 		log.Fatal("Error seeding users at db")
 	}
 
+}
+
+func seedStatus(db *gorm.DB) {
+	db.AutoMigrate(&model.Status{})
+	var status []model.Status
+
+	statusSend := model.Status{Description: os.Getenv("STATUS_SEND")}
+	statusSendId, err1 := strconv.ParseUint(os.Getenv("STATUS_SEND_CODE"), 10, 64)
+	statusSend.ID = uint(statusSendId)
+
+	statusCla1 := model.Status{Description: os.Getenv("STATUS_CLASIFIED1")}
+	statusCla1Id, err2 := strconv.ParseUint(os.Getenv("STATUS_CLASIFIED1_CODE"), 10, 64)
+	statusCla1.ID = uint(statusCla1Id)
+
+	statusCla2 := model.Status{Description: os.Getenv("STATUS_CLASIFIED2")}
+	statusCla2Id, err3 := strconv.ParseUint(os.Getenv("STATUS_CLASIFIED2_CODE"), 10, 64)
+	statusCla2.ID = uint(statusCla2Id)
+
+	statusAdmin := model.Status{Description: os.Getenv("STATUS_ADMIN")}
+	statusAdminId, err4 := strconv.ParseUint(os.Getenv("STATUS_ADMIN_CODE"), 10, 64)
+	statusAdmin.ID = uint(statusAdminId)
+
+	statusFinish := model.Status{Description: os.Getenv("STATUS_FINISH")}
+	statusFinishId, err5 := strconv.ParseUint(os.Getenv("STATUS_FINISH_CODE"), 10, 64)
+	statusFinish.ID = uint(statusFinishId)
+
+	status = append(status, statusSend)
+	status = append(status, statusCla1)
+	status = append(status, statusCla2)
+	status = append(status, statusAdmin)
+	status = append(status, statusFinish)
+
+	err6 := db.Create(&status).Error
+
+	if atLeastOneError(err1, err2, err3, err4, err5, err6) {
+		log.Fatal("Error seeding status")
+	}
+}
+
+func atLeastOneError(errors ...error) bool {
+	for _, err := range errors {
+		if err != nil {
+			return true
+		}
+	}
+
+	return false
 }
