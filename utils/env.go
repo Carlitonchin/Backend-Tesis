908ddce08359164ff8b72f85058d869b41e4a1b3
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-03 15:05:02 -0300

    change package name

--- utils/env.go
+++ utils/env.go
@@ -1,11 +1,11 @@
-package myUtils
+package main_utils
 
 import (
 	"os"
 	"strconv"
 )
 
-func getUintEnv(key string) (uint, error) {
+func GetUintEnv(key string) (uint, error) {
 	ui, err := strconv.ParseUint(os.Getenv(key), 10, 64)
 
 	return uint(ui), err
