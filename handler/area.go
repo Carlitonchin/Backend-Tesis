678a04df6e8cfab095df65a2c959d235c15a075f
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-04 16:22:03 -0300

    add area handler

---
+++ handler/area.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Carlitonchin/Backend-Tesis/handler/handler_utils"
+	"github.com/Carlitonchin/Backend-Tesis/model"
+	"github.com/gin-gonic/gin"
+)
+
+type areaReq struct {
+	Name string `json:"name" binding:"required"`
+}
+
+func (h *Handler) AddArea(ctx *gin.Context) {
+	var req areaReq
+	if ok := bindData(ctx, &req); !ok {
+		return
+	}
+
+	area := &model.Area{
+		Name: req.Name,
+	}
+
+	area, err := h.AreaService.AddArea(ctx.Request.Context(), area)
+
+	if err != nil {
+		handler_utils.SendErrorResponse(ctx, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"area": area.ID,
+	})
+}
