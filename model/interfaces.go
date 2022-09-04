7b1356674feea30e277146c60c487c163cd3253a
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-04 16:31:04 -0300

    add UpdateUserArea to user service and repository interfaces

--- model/interfaces.go
+++ model/interfaces.go
@@ -12,6 +12,7 @@ type UserService interface {
 	GetAllUsers(ctx context.Context) ([]User, error)
 	AddRoleToUser(ctx context.Context, user_id uint, role_id uint) error
 	AddQuestion(ctx context.Context, question *Question) (*Question, error)
+	UpdateUserRole(ctx context.Context, user_id uint, area_id uint) error
 }
 
 type UserRepository interface {
@@ -21,6 +22,7 @@ type UserRepository interface {
 	GetAllUsers(ctx context.Context) ([]User, error)
 	AddRoleToUser(ctx context.Context, user_id uint, role_id uint) error
 	CreateQuestion(ctx context.Context, question *Question) (*Question, error)
+	UpdateUserArea(ctx context.Context, user_id uint, area_id uint) error
 }
 
 type TokenService interface {
