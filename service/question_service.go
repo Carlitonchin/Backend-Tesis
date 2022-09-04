d82c6c84d8dfe62e3c76dd35ee49384f45b6cbac
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-04 20:54:45 -0300

    move question status to service layer

--- service/question_service.go
+++ service/question_service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/Carlitonchin/Backend-Tesis/model"
+	"github.com/Carlitonchin/Backend-Tesis/model/apperrors"
+	"github.com/Carlitonchin/Backend-Tesis/some_utils"
 )
 
 type questionService struct {
@@ -20,5 +22,15 @@ func (s *questionService) AddQuestion(
 	ctx context.Context, question *model.Question) (
 	*model.Question, error) {
 
+	status_send_id, err := some_utils.GetUintEnv("STATUS_SEND_CODE")
+	if err != nil {
+		type_error := apperrors.Internal
+		message := "No se pudo leer el id del stado 'enviada'"
+
+		e := apperrors.NewError(type_error, message)
+		return nil, e
+	}
+
+	question.StatusId = status_send_id
 	return s.repo.CreateQuestion(ctx, question)
 }
