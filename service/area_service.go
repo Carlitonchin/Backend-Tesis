07a27534382140c7c504b562295149e3a97729ef
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-04 20:13:05 -0300

    add Clasify area service implementation

--- service/area_service.go
+++ service/area_service.go
@@ -19,3 +19,7 @@ func NewAreaService(area_repo model.AreaRepository) model.AreaService {
 func (s *areaService) AddArea(ctx context.Context, area *model.Area) (*model.Area, error) {
 	return s.repo.CreateArea(ctx, area)
 }
+
+func (s *areaService) Clasify(ctx context.Context, question_id uint, area_id uint) error {
+	return s.repo.Clasify(ctx, question_id, area_id)
+}
