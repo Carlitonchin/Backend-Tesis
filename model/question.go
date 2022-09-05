f62ab8c140ca7dfc7405fab5372f8a8b7df4fcca
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-04 21:49:58 -0300

    add user-question responsible relationship

--- model/question.go
+++ model/question.go
@@ -4,9 +4,10 @@ import "gorm.io/gorm"
 
 type Question struct {
 	gorm.Model
-	Text      string  `json:"text" gorm:"not null"`
-	UserRefer uint    `json:"user_id"`
-	StatusId  uint    `json:"status_id"`
-	Status    *Status `json:"status" gorm:"-"`
-	AreaID    *uint   `json:"area_id"`
+	Text            string  `json:"text" gorm:"not null"`
+	UserRefer       uint    `json:"user_id"`
+	StatusId        uint    `json:"status_id"`
+	Status          *Status `json:"status" gorm:"-"`
+	AreaID          *uint   `json:"area_id"`
+	UserResponsible *uint   `json:"user_responsible_id"`
 }
