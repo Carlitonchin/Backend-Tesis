fb240924a856f71e8cc16daae049f58e7cf0a81e
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-04 21:44:14 -0300

    add logic for invalidate clasification of already clasified questions

--- service/question_service.go
+++ service/question_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Carlitonchin/Backend-Tesis/model"
 	"github.com/Carlitonchin/Backend-Tesis/model/apperrors"
@@ -36,5 +37,23 @@ func (s *questionService) AddQuestion(
 }
 
 func (s *questionService) Clasify(ctx context.Context, question_id uint, area_id uint) error {
+	question, err := s.repo.GetById(ctx, question_id)
+	if err != nil {
+		return err
+	}
+
+	status_sended_id, err := some_utils.GetUintEnv("STATUS_SEND_CODE")
+	if err != nil {
+		return err
+	}
+
+	if question.StatusId != status_sended_id {
+		type_error := apperrors.Conflict
+		message := fmt.Sprintf("Solo se pueden clasificar de esa manera las preguntas con status_id = '%v'", status_sended_id)
+
+		e := apperrors.NewError(type_error, message)
+		return e
+	}
+
 	return s.repo.Clasify(ctx, question_id, area_id)
 }
