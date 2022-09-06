8a0ff6a8e47b9c47b98df3cc62123999875df376
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-06 11:06:53 -0300

    fix error, admin can take all questions

--- service/question_service.go
+++ service/question_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/Carlitonchin/Backend-Tesis/model"
 	"github.com/Carlitonchin/Backend-Tesis/model/apperrors"
@@ -65,6 +66,8 @@ func (s *questionService) TakeQuestion(ctx context.Context, user *model.User, qu
 		return err
 	}
 
+	rol_admin := os.Getenv("ROLE_ADMIN")
+
 	question, err := s.repo.GetById(ctx, question_id)
 	if err != nil {
 		return err
@@ -84,7 +87,7 @@ func (s *questionService) TakeQuestion(ctx context.Context, user *model.User, qu
 		return err
 	}
 
-	if user.AreaID == nil || *user.AreaID != *question.AreaID {
+	if user.Role.Name != rol_admin && (user.AreaID == nil || *user.AreaID != *question.AreaID) {
 		type_error := apperrors.Conflict
 		message := fmt.Sprintf("La pregunta con id = '%v' solo puede ser tomada por usuarios del area de id = '%v'", question_id, *question.AreaID)
 		err = apperrors.NewError(type_error, message)
