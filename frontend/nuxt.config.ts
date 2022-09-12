590e9563388248aee9f486e2363ec186b8acbadd
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-12 11:49:11 -0300

    delete frontend project

--- frontend/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-    css:['~/assets/css/main.css',
-     '~/assets/css/flex-layout.css',
-     '~/assets/css/margin.css',
-     '~/assets/css/button.css']
-})
--- frontend/pages/index.vue
+++
@@ -1,6 +0,0 @@
-
-<template>
-<NuxtLayout page="Inicio" title="Ask Matcom | Home">
-  <PageHome/>
-</NuxtLayout>
-</template>
