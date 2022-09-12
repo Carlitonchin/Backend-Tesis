5f1ff6b5d05ffe7ac32b0146f8f9b2ddf7d05796
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-12 15:21:39 -0300

    add validation error component

---
+++ frontend/components/ValidationError.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import { ErrorMessage } from 'vee-validate';
+    const {field} = defineProps(["field"])
+</script>
+
+<template>
+    <ErrorMessage as="p" v-slot="{message}" :name="field">
+           {{". " + message}}
+            </ErrorMessage>
+</template>
