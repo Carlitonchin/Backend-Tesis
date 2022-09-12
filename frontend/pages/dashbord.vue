006feff85589e6308ab708a656d39750473af96e
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-12 17:39:39 -0300

    add dashbord page

---
+++ frontend/pages/dashbord.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const user = useCookie("user").value
+    if(!user){
+        navigateTo("/")
+    }
+    
+</script>
+
+<template>
+    <div class="flex flex-col space-y-10 justify-center items-center">
+    <h2 class="text-secondary">Hola <span class="text-red-500">{{user.name}}</span></h2>
+
+</div>
+</template>
