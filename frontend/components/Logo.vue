ff131cc688ff7b139ee35b17752c9255e9c84b1c
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-12 13:18:36 -0300

    add logo

---
+++ frontend/components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+<h1 class="font-brand text-primary"><span class="text-red-500">Ask</span>MATCOM</h1>
+</template>
--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -2,7 +2,7 @@
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
   <div class="w-full max-w-md space-y-8">
     <div>
-      
+        <Logo/>
     </div>
     <form class="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" value="true">
