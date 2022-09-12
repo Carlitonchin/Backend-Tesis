a1f19fb406262283f0bc49a272af7fef86f763bb
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-12 17:39:29 -0300

    get user from cookie

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+    import jwtDecode from 'jwt-decode'
+    const tokens_cookie = useCookie("tokens").value
+    let user = null
+    if(tokens_cookie){
+        user = jwtDecode(tokens_cookie["idToken"]).user
+        useCookie("user").value = user
+        navigateTo("/dashbord")
+    }
+    
+</script>
+
 <template>
-    <NuxtLink to="/signup">Registrarse</NuxtLink>
+    <div v-if="!user">
+    <NuxtLink to="/signup" >Registrarse</NuxtLink>
+    </div>
 </template>
