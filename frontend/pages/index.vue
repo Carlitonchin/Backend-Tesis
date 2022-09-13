73b0c9c19f4b82120d07a5ee93b31bb76f43d026
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 17:02:37 -0300

    remove middleware function for index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,8 @@
-<script setup>
-    import jwtDecode from 'jwt-decode'
-    const tokens_cookie = useCookie("tokens").value
-    let user = null
-    if(tokens_cookie){
-        user = jwtDecode(tokens_cookie["idToken"]).user
-        useCookie("user").value = user
-        navigateTo("/dashbord")
-    }
-    
-</script>
 
 <template>
-    <div v-if="!user">
+    <NuxtLayout>
+    <div>
     <h1>Bienvenidos</h1>
     </div>
+</NuxtLayout>
 </template>
