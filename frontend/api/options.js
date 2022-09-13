be13c38aa9bd0a09fa69336dd0d8d9575e64bab4
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 12:46:30 -0300

    add const for take roles names

--- frontend/api/options.js
+++ frontend/api/options.js
@@ -1,4 +1,9 @@
 export const API_URL = "http://172.18.0.5:8080/"
+export const STUDENT_ROLE = "Estudiante"
+export const WORKER_ROLE = "Clasificador"
+export const ADMIN_ROLE = "Administrador"
+export const LEVEL1_SPECIALIST = "Especialista Nivel 1"
+export const LEVEL2_SPECIALIST = "Especialista Nivel 2"
 
 export const HEADERS = {
     'Content-Type': 'application/json'
