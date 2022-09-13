7bb5de263fb2a81e2aa42836f6d0f7fd433e741a
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 13:14:23 -0300

    add authorization to question api

--- frontend/api/question.js
+++ frontend/api/question.js
@@ -2,13 +2,17 @@ import { API_URL, HEADERS } from "./options";
 
 const PATH = "api/questions/add"
 
-export default async (body)=>{
+export default async (tokens,body)=>{
+    const headers = {
+        ...HEADERS,
+        'Authorization': `Bearer ${tokens["idToken"]}`, 
+    }
+    console.log(headers)
     let response = await fetch(API_URL + PATH,{
         method:"POST",
-        headers:HEADERS,
+        headers:headers,
         body:JSON.stringify(body)
     })
-
     response = await response.json()
     return response
 }
