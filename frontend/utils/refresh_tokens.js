a612dcb08132f6e22ab3f5881a041f363a8caf41
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 17:47:58 -0300

    redirect to index if cant refresh tokens

--- frontend/utils/refresh_tokens.js
+++ frontend/utils/refresh_tokens.js
@@ -1,12 +1,13 @@
 import {UNAUTH_STATUS} from '../api/options'
 import post_token from '../api/tokens'
+import remove_user_cookie from './remove_user_cookie'
 
 export default async (response, tokens, fetcher, value)=>{
     if(response.status == UNAUTH_STATUS){
         response = await post_token(tokens)
-        response = await response.json()
         if(response.error){
-            alert(response.error)
+            remove_user_cookie()
+            window.location.href("/")
             return
         }
 
@@ -15,5 +16,5 @@ export default async (response, tokens, fetcher, value)=>{
         return await response.json()
     }
 
-    return response
+    return await response.json()
 }
