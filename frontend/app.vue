061609b30d4cd890fe1b45223179d1ac10c99c2e
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 14:23:50 -0300

    add nav bar to app

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,24 @@
+<script setup>
+import Logo from './components/Logo.vue';
+  const user = useCookie("user").value
+</script>
 <template>
-  <div class="bg-gray-900 text-white min-h-screen min-w-full">
+  <section class="bg-gray-900 text-white min-h-screen min-w-full">
+    <nav class="bg-gray-800 shadow-md flex p-4 justify-between items-center">
+      <div>
+      <Logo/>
+    </div>
+    <div>
+
+    </div>
+    <div>
+      <button v-if="user" class="primary-button">Cerrar Sesion</button>
+      <div v-else>
+        <NuxtLink>Inciar sesión</NuxtLink>
+        <NuxtLink>Registrarse</NuxtLink>
+      </div>
+    </div>
+    </nav>
     <NuxtPage />
-  </div>
+  </section>
 </template>
