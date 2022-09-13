88d2487be27763835e5657fcba4b75df9bd357d7
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 17:01:28 -0300

    move nav bar to middleware

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,24 +1,6 @@
-<script setup>
-import Logo from './components/Logo.vue';
-  const user = useCookie("user").value
-</script>
+
 <template>
   <section class="bg-gray-900 text-white min-h-screen min-w-full">
-    <nav class="bg-gray-800 shadow-md flex p-4 justify-between items-center">
-      <div>
-      <Logo/>
-    </div>
-    <div>
-
-    </div>
-    <div>
-      <button v-if="user" class="primary-button">Cerrar Sesion</button>
-      <div v-else>
-        <NuxtLink>Inciar sesión</NuxtLink>
-        <NuxtLink>Registrarse</NuxtLink>
-      </div>
-    </div>
-    </nav>
     <NuxtPage />
   </section>
 </template>
