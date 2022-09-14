ef881070bc6839b48cc090addf2e592b839bf8e8
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 22:39:50 -0300

    move navbar to a single component

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,54 +1,9 @@
-<script setup>
-    import Logo from '../components/Logo.vue';
-    import logout_post from '../api/logout'
-    import refresh_tokens from '../utils/refresh_tokens';
-    import remove_user_cookie from '../utils/remove_user_cookie';
-    
-      const user = useCookie("user").value
-    
-      const tokens = useCookie("tokens").value
 
-      async function logout_handler()
-      {
-        let response = await logout_post(tokens)
-        response = await refresh_tokens(response, tokens, logout_post, null)
-    
-        if(response.error){
-          alert(response.error)
-          return
-        }
-    
-        remove_user_cookie()
-        window.location.href="/"
-    
-      }
-    </script>
 
 <template>
-     
-    <nav class="bg-gray-800 shadow-md flex flex-col space-y-2 p-4 justify-between items-center">
-      <div>
-      <Logo/>
-    </div>
-    <div>
-
-    </div>
-    <div>
-      <button v-if="user" @click="logout_handler" class="primary-button">Cerrar Sesion</button>
-      <div v-else class="flex space-x-2">
-        <a href="signin">
-        <button class="secondary-button">
-          Inciar sesión
-        </button>
-          
-    </a>
-        <a href="/signup">
-          <button class="primary-button">
-          Registrarse
-        </button>
-    </a>
-      </div>
-    </div>
-    </nav>
+     <div class="flex flex-col space-y-5">
+    <NavBar/>
+      
     <slot/>
+  </div>
 </template>
