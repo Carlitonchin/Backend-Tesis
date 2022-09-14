6e41901d3430457e32d2df384be0ea3de9d78c9a
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 23:57:12 -0300

    add my-quesitions page

---
+++ frontend/pages/dashbord/my-questions.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import {STUDENT_ROLE} from '../../api/options'
+import StudentQuestions from '../../components/dashbord/StudentQuestions.vue'
+definePageMeta({
+  middleware: ["index"]
+})
+
+const user = useCookie("user").value
+const role = user.role.name
+</script>
+
+<template>
+    <NuxtLayout>
+        <StudentQuestions v-if="role == STUDENT_ROLE"/>
+</NuxtLayout>
+</template>
