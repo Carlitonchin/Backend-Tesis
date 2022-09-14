04e8e7b3c6efe69483c98c6341bb2d4fa2e4f4b2
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-14 00:30:36 -0300

    student question component almost finished

--- frontend/components/dashbord/StudentQuestions.vue
+++ frontend/components/dashbord/StudentQuestions.vue
@@ -1,7 +1,31 @@
 <script setup>
-    
+    import get_questions from '../../api/student_questions'
+    import refresh_tokens from '~~/utils/refresh_tokens';
+    const tokens = useCookie("tokens").value
+    const questions = ref([])
+    onMounted(async ()=>{
+        let response = await get_questions(tokens)
+        response = await refresh_tokens(response, tokens, get_questions, null)
+        if(response.error){
+            alert(response.error)
+            return
+        }
+
+        questions.value = response.questions
+    })
 </script>
 
 <template>
-    hola
+    <div class="p-6">
+        <h2 class="text-2xl font-semibold mb-4">Mis dudas:</h2>
+        <div class="space-y-3">
+        <div v-for="question,i in questions">
+            <p><span class="font-semibold text-red-500">{{(i+1) + ". "}}</span> {{question.text}}</p>
+            <div class="flex items-center">
+                <p class="text-red-500" v-if="question.response">{{"Respuesta: " + question.response}}</p>
+            <p v-else><span class="text-red-500 m-0">Respuesta:</span> -</p>
+            </div>
+        </div>
+    </div>
+    </div>
 </template>
