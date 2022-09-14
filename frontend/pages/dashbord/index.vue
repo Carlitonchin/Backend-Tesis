755f104166c2f719c28fbb9b2f547c4359e37742
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-13 22:44:40 -0300

    create dashbord folder for multiple pages

--- frontend/pages/dashbord/index.vue
+++ frontend/pages/dashbord/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Student from "../components/dashbord/Student.vue";
-import {STUDENT_ROLE} from '../api/options'
+import Student from "../../components/dashbord/Student.vue";
+import {STUDENT_ROLE} from '../../api/options'
 const tokens = useCookie("tokens").value
     const user = useCookie("user").value
     if(!user || !tokens){
