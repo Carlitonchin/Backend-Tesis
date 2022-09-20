1434c6ec197a3683315622b2bb86559c380e1a42
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-20 11:34:40 -0300

    draw unclasified questions at unclasifiedQuestions component

--- frontend/components/dashbord/UnclasifiedQuestions.vue
+++ frontend/components/dashbord/UnclasifiedQuestions.vue
@@ -1,16 +1,24 @@
+<script setup>
+    import {API_URL, HEADERS} from '../../api/options'
+    import get_data from '../../api/get_unclasified_questions'
+    import refresh_tokens from '~~/utils/refresh_tokens';
+
+    const tokens = useCookie("tokens").value
+    let response = await get_data(tokens)
+    response = await refresh_tokens(response, tokens, get_data, null)
+    if(response.error)
+        alert(response.error)
+    
+    const questions = ref(response.questions)
+</script>
+
 <template>
     <div class="p-6 w-full">
     <h2 class="text-secondary max-w-full">Preguntas sin clasificar</h2>
     <div class="space-y-4 mt-5">
-        <div class="flex space-x-3">
+        <div v-for="question in questions" class="flex space-x-3">
         <div class="leading-6">
-            Pregunta 1 larga para asdfsadf asdfsad asfdasdf ver como queda pues
-        </div>
-        <ThreePointOptions/>
-    </div>
-    <div>
-        <div>
-            Pregunta 2
+           {{question.text}}
         </div>
         <ThreePointOptions/>
     </div>
