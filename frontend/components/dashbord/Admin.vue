79a2aa4a1a09b7e12407fcbe6c826a0c87c08be7
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-20 16:56:36 -0300

    add admin main page

--- frontend/components/dashbord/Admin.vue
+++ frontend/components/dashbord/Admin.vue
@@ -1,12 +1,11 @@
 <script setup>
     const {user, tokens} = defineProps(['user', 'tokens'])
-    const options = [[{text:"Áreas", href:"/dashbord/areas"}]]
 </script>
 
 <template>
-    <div class="w-full flex flex-col space-y-4">
-        <Options :options="options"/>
-        <h1 class="text-primary">Hola {{user.name}}</h1>
-    </div>
+    <NuxtLayout name="admin">
+        <h1>Hola {{user.name}}</h1>
+    </NuxtLayout>
+
     
 </template>
