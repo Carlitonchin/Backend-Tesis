58eb0f6165e06d37057f168a20a670b6be5cb74f
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-20 16:56:50 -0300

    working at areas page, pending create fetcher

---
+++ frontend/pages/dashbord/area.vue
@@ -0,0 +1,49 @@
+<script setup>
+    import { Form, Field } from 'vee-validate';
+import get_areas from '~~/api/get_areas';
+import refresh_tokens from '~~/utils/refresh_tokens';
+
+    const tokens = useCookie("tokens").value
+    const areas = ref([])
+    let response = await get_areas(tokens)
+    response = await refresh_tokens(response, tokens, get_areas, null)
+    if(response.error)
+        console.log(response.error)
+    else{
+        areas.value = response.areas
+    }
+</script>
+<template>
+    <NuxtLayout>
+        <NuxtLayout name="admin">
+            <div class="pl-2 pr-2">
+                <Form class="space-y-1 w-full" action="#" method="POST" @submit="handle_submit">
+      <input type="hidden" name="remember" value="true">
+      <div class="-space-y-px rounded-md shadow-sm w-full">
+        <div>
+          <label for="area" class="sr-only">Nueva Área</label>
+          <Field id="area" name="area" type="text" autocomplete="name" required 
+          class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2
+           text-gray-900 placeholder-gray-500 focus:z-10 focus:border-red-500 focus:outline-none
+            focus:ring-red-500 sm:text-sm" placeholder="Nueva Área"/>
+           
+        </div>
+       
+       
+      </div>
+      <div>
+        <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+          Crear
+        </button>
+      </div>
+      
+    </Form>
+
+    <h2 class="text-primary mt-4">Áreas</h2>
+    <p v-for="area in areas">
+    {{area.name}}
+    </p>
+            </div>
+        </NuxtLayout>
+    </NuxtLayout>
+</template>
