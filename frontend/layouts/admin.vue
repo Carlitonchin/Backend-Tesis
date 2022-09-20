3ced4e85018dd952611dd2ac09eb639b33cb547d
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-20 16:56:23 -0300

    add admin template

---
+++ frontend/layouts/admin.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const options = [[{text:"Áreas", href:"/dashbord/area"}]]
+</script>
+
+<template>
+    <div class="w-full flex flex-col space-y-4">
+        <Options :options="options"/>
+        <slot/>
+    </div>
+</template>
