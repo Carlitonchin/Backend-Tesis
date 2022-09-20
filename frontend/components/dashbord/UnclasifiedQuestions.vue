8d6fc57e9cb8c042f67786bf6348f818c47cbffb
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-20 12:15:00 -0300

    fetch areas at unclasifiedQuestions component

--- frontend/components/dashbord/UnclasifiedQuestions.vue
+++ frontend/components/dashbord/UnclasifiedQuestions.vue
@@ -1,15 +1,23 @@
 <script setup>
     import {API_URL, HEADERS} from '../../api/options'
-    import get_data from '../../api/get_unclasified_questions'
+    import get_questions from '../../api/get_unclasified_questions'
+    import get_areas from '~~/api/get_areas';
     import refresh_tokens from '~~/utils/refresh_tokens';
 
     const tokens = useCookie("tokens").value
-    let response = await get_data(tokens)
-    response = await refresh_tokens(response, tokens, get_data, null)
-    if(response.error)
-        alert(response.error)
+    let response_questions = await get_questions(tokens)
+    response_questions = await refresh_tokens(response_questions, tokens, get_questions, null)
+    if(response_questions.error)
+        console.log(response_questions.error)
     
-    const questions = ref(response.questions)
+    let response_areas = await get_areas(tokens)
+    response_areas = await refresh_tokens(response_areas, tokens, get_areas, null)
+    if(response_areas.error)
+        console.log(response_areas.error)
+    
+    console.log(response_areas.areas)
+    const questions = ref(response_questions.questions)
+    const areas = ref(response_areas.areas)
 </script>
 
 <template>
