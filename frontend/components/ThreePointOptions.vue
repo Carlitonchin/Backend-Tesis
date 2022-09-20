f2ba6af4026e0bf0fde4d4b38ff707f7c30a45de
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-20 15:22:59 -0300

    unclasifiedquestion component almost finished, pending clasify post req

--- frontend/components/ThreePointOptions.vue
+++ frontend/components/ThreePointOptions.vue
@@ -1,9 +1,34 @@
+<script setup>
+    const {options, handle_click} = defineProps(['options', 'handle_click'])
+    const focus = ref(false)
+    
+    const take_focus = ()=>{
+        focus.value = !focus.value
+    }
+
+    function exit_focus(){
+        setTimeout(()=>{
+            focus.value = false
+        }, 200)
+    }
+</script>
+
 <template>
-    <div class="flex space-x-1 h-fit w-fit p-2 cursor-pointer red-animation">
+    <div>
+    <button class="flex space-x-1 h-fit w-fit p-2 cursor-pointer red-animation" @click="take_focus" @blur="exit_focus">
         <div class="w-2 h-2 bg-gray-50 rounded-full child"></div>
         <div class="w-2 h-2 bg-gray-50 rounded-full child"></div>
         <div class="w-2 h-2 bg-gray-50 rounded-full child"></div>
+    </button>
+    <div :class="['absolute', 'text-white','z-10','w-fit','origin-top-right','divide-y','divide-gray-100',
+'bg-gray-800', 'shadow-sm','ring-1','ring-black','ring-opacity-5','focus:outline-none',
+!focus ? 'hidden' : 'block']" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+    <div >
+      <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
+      <a v-for="opt in options" @click="handle_click(opt.ID)" class="block cursor-pointer px-4 py-2 text-sm hover:bg-gray-500">{{opt.name}}</a>
     </div>
+  </div>
+</div>
 </template>
 
 <style scoped>
--- frontend/components/dashbord/UnclasifiedQuestions.vue
+++ frontend/components/dashbord/UnclasifiedQuestions.vue
@@ -4,32 +4,48 @@
     import get_areas from '~~/api/get_areas';
     import refresh_tokens from '~~/utils/refresh_tokens';
 
-    const tokens = useCookie("tokens").value
+    const {user, tokens} = defineProps(["user", "tokens"])
+    const questions = ref(null)
+    const areas = ref(null)
+    
+    function clasify(question_id, area_id){
+        console.log(question_id, area_id)
+    }
+
+   onMounted(async ()=>{
     let response_questions = await get_questions(tokens)
     response_questions = await refresh_tokens(response_questions, tokens, get_questions, null)
-    if(response_questions.error)
+    if(response_questions.error){
         console.log(response_questions.error)
-    
+        return
+    }
+
     let response_areas = await get_areas(tokens)
     response_areas = await refresh_tokens(response_areas, tokens, get_areas, null)
-    if(response_areas.error)
+    if(response_areas.error){
         console.log(response_areas.error)
+        return
+    }
     
-    console.log(response_areas.areas)
-    const questions = ref(response_questions.questions)
-    const areas = ref(response_areas.areas)
+    questions.value = response_questions.questions
+    areas.value = response_areas.areas
+   })
+    
 </script>
 
 <template>
     <div class="p-6 w-full">
     <h2 class="text-secondary max-w-full">Preguntas sin clasificar</h2>
-    <div class="space-y-8 mt-5">
+    <div class="space-y-8 mt-5" v-if="areas && questions">
         <div v-for="question in questions" class="flex space-x-3">
         <div class="leading-6">
            {{question.text}}
         </div>
-        <ThreePointOptions/>
+        <ThreePointOptions :options="areas" :handle_click="(area_id) => clasify(question.ID, area_id)"/>
     </div>
+    </div>
+    <div v-else>
+        Cargando ...
     </div>
 </div>
 </template>
