4389d0f6a34ba5dd0f47bbb1221b1963362426db
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-21 14:17:10 -0300

    add link to home for logo

--- frontend/components/Logo.vue
+++ frontend/components/Logo.vue
@@ -1,3 +1,6 @@
 <template>
-<h1 class="font-brand text-primary"><span class="text-red-500">Ask</span>MATCOM</h1>
+    <NuxtLink to="/">
+
+        <h1 class="font-brand text-primary"><span class="text-red-500">Ask</span>MATCOM</h1>
+    </NuxtLink>
 </template>
