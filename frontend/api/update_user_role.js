a3b79cfef77d864c4cf4e38636ca6db8e9e407f0
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-21 10:31:05 -0300

    add update user role fetcher

---
+++ frontend/api/update_user_role.js
@@ -0,0 +1,16 @@
+import { API_URL, HEADERS } from "./options";
+
+const PATH = "api/users/update-role"
+
+export default async (tokens,body)=>{
+    const headers = {
+        ...HEADERS,
+        'Authorization': `Bearer ${tokens["idToken"]}`, 
+    }
+    let response = await fetch(API_URL + PATH,{
+        method:"PUT",
+        headers:headers,
+        body:JSON.stringify(body)
+    })
+    return response
+}
