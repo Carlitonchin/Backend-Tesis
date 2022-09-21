8f127f6289337b48e90cf8ef88e72fe98174f5a5
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-21 14:17:19 -0300

    create question by status page

---
+++ frontend/pages/dashbord/questions-[status]/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+const status_code = useRoute().params.id
+if(!status_code)
+    console.log("status not especified")
+</script>
+
+<template>
+    <h1>Holita {{route.params.status}}</h1>
+</template>
