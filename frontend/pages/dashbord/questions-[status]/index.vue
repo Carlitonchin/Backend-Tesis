dc52991621a90fa6973a1e0e0391b991356b14fd
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-22 11:09:19 -0300

    working at questions view

--- frontend/pages/dashbord/questions-[status]/index.vue
+++ frontend/pages/dashbord/questions-[status]/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import get_question from '~~/api/get_question_by_status'
 import my_fetch from '~~/utils/my_fetch';
+import {ADMIN_ROLE, QuestionsStatusDict} from '~~/api/options'
 
 definePageMeta({
   middleware: ["index"]
@@ -10,9 +11,13 @@ const status_code = useRoute().params.status
 if(!status_code)
     console.log("status not especified")
 
+if(!QuestionsStatusDict[status_code]){
+    console.log(`status code ${status_code} no reconocido`)
+    window.location.href="/"
+}
 
 const tokens = useCookie("tokens").value
-
+const user = useCookie("user").value
 const questions = ref([])
 
 let response = await my_fetch(tokens, get_question, status_code)
@@ -24,16 +29,34 @@ else
 </script>
 
 <template>
+    <NuxtLayout>
+        <NuxtLayout name="admin" v-if="user.role.name === ADMIN_ROLE">
     <div class="p-6 w-full">
-    <h2 class="text-secondary max-w-full">Preguntas clasificadas</h2>
+    <h2 class="text-secondary max-w-full">{{QuestionsStatusDict[status_code]}}</h2>
+    <h3 v-if="user.role.name !== ADMIN_ROLE" class="text-xl">Área: {{user.area.name}}</h3>
     <div class="space-y-8 mt-5">
         <div v-if="questions.length" v-for="question in questions" class="flex space-x-3">
         <div class="leading-6">
            {{question.text}}
         </div>
-        <ThreePointOptions :options="areas" :handle_click="(area_id) => clasify(question.ID, area_id)"/>
     </div>
     <p v-else>No hay</p>
     </div>
 </div>
+</NuxtLayout>
+
+<div v-else class="p-6 w-full">
+    <h2 class="text-secondary max-w-full">{{QuestionsStatusDict[status_code]}}</h2>
+    <h3 class="text-xl">Área: {{user.area.name}}</h3>
+    <div class="space-y-8 mt-5">
+        <div v-if="questions.length" v-for="question in questions" class="flex space-x-3">
+        <div class="leading-6">
+           {{question.text}}
+        </div>
+    </div>
+    <p v-else>No hay</p>
+    </div>
+</div>
+
+</NuxtLayout>
 </template>
