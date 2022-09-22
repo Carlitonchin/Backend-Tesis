d37e8f18fcd927d5068726f0efc36fab85af7573
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-22 17:42:58 -0300

    change token number

--- frontend/api/tokens.js
+++ frontend/api/tokens.js
@@ -2,13 +2,14 @@ import { API_URL, HEADERS } from "./options";
 const PATH = "api/account/tokens"
 
 export default async (tokens)=>{
-    const body = {
-        "refreshToken":tokens['refreshToken']
+    const rf = {
+        refreshToken:tokens['refreshToken']
     }
+
     let response = await fetch(API_URL + PATH,{
         method:"POST",
         headers:HEADERS,
-        body:JSON.stringify(body)
+        body:JSON.stringify(rf)
     })
     response = await response.json()
     return response
