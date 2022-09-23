1aa9c3e46fec8cb894dae2b6fefdac0c899da370
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-23 09:31:14 -0300

    add another middleware for not give permission to especified roles

--- backend/handler/middleware/only_role.go
+++ backend/handler/middleware/only_role.go
@@ -43,3 +43,39 @@ func OnlyRoles(roles []string) gin.HandlerFunc {
 		ctx.Next()
 	}
 }
+
+func NotTheseRoles(roles []string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		user, err := handler_utils.GetUser(ctx)
+		if err != nil {
+			ctx.JSON(apperrors.Status(err), gin.H{
+				"error": err,
+			})
+			ctx.Abort()
+			return
+		}
+
+		fail := false
+
+		for _, role := range roles {
+			if user.Role.Name == os.Getenv(role) {
+				fail = true
+			}
+		}
+
+		if fail {
+			type_error := apperrors.Authorization
+			message := "No tiene permisos para acceder a este recurso"
+
+			e := apperrors.NewError(type_error, message)
+			ctx.JSON(e.Status(), gin.H{
+				"error": e,
+			})
+
+			ctx.Abort()
+			return
+		}
+
+		ctx.Next()
+	}
+}
