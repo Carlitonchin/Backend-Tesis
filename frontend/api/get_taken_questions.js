cb1fb5c8b42c9b34314e9c32d8c1cf9449dcbc47
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-23 11:15:54 -0300

    add get taken questions fetcher

---
+++ frontend/api/get_taken_questions.js
@@ -0,0 +1,15 @@
+import { API_URL, HEADERS } from "./options";
+
+const PATH = "api/questions/taken"
+
+export default async (tokens)=>{
+    const headers = {
+        ...HEADERS,
+        'Authorization': `Bearer ${tokens["idToken"]}`, 
+    }
+    let response = await fetch(API_URL + PATH,{
+        method:'GET',
+        headers:headers
+    })
+    return response
+}
