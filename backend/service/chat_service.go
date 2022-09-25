2c4ac2d596a7a8bd55b9f90398b59bcf62358b35
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-25 17:58:06 -0300

    add chatService implementation

---
+++ backend/service/chat_service.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Carlitonchin/Backend-Tesis/model"
+)
+
+type chatService struct {
+	ChatRepository model.ChatRepository
+}
+
+func NewChatService(chat_repo model.ChatRepository) model.ChatService {
+	return &chatService{
+		ChatRepository: chat_repo,
+	}
+}
+
+func (s *chatService) SendMessage(ctx context.Context, question_id uint, user_id uint, text string) error {
+	return s.ChatRepository.SendMessage(ctx, question_id, user_id, text)
+}
+
+func (s *chatService) GetMessages(ctx context.Context, question_id uint, user_id uint) ([]model.MessageChat, error) {
+	messages, err := s.ChatRepository.GetMessages(ctx, question_id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.ChatRepository.ReadMessages(ctx, question_id, user_id)
+
+	return messages, err
+}
