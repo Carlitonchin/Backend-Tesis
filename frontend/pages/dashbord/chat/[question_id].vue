ae9d6cc7f6829292acc9c436057e4196b1a1096c
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-26 15:42:14 -0300

    send message from chat view

--- frontend/pages/dashbord/chat/[question_id].vue
+++ frontend/pages/dashbord/chat/[question_id].vue
@@ -1,6 +1,7 @@
 <script setup>
 import my_fetch from '~~/utils/my_fetch';
 import get_messages from '~~/api/get_messages';
+import add_message from '~~/api/add_message'
 
 definePageMeta({
   middleware: ["index"]
@@ -37,8 +38,6 @@ else{
     question.value = response.question
 }
 
-
-
 const messages_ordered = computed(()=>{
     return messages.value.sort((a,b)=>{
         if(a.CreatedAt <= b.CreatedAt)
@@ -63,26 +62,44 @@ onMounted(()=>{
     f()
 })
 
+async function send_message(){
+    let text_input = document.getElementById("text-input")
+    let text = text_input.value
+    let body = {question_id, text}
+    let response = await my_fetch(tokens, add_message, body)
+
+    if(response.error){
+        console.log(response.error)
+        return
+    }
+
+    text_input.value = ""
+    messages.value.push(response.message)
+}
+
 </script>
 
 <template>
     <NuxtLayout name="logged">
         <div class="p-4 pt-0 space-y-4">
         <h2 class="text-secondary">{{question.text}}</h2>
-        <form class="w-full">
+        
+        <div>
+            <ChatBubble :messages="messages_ordered"/>
+        </div>
+
+        <form class="w-full" @submit.prevent="send_message">
             <div class="w-full flex">
-            <input class="relative block w-full appearance-none rounded-md rounded-r-none border
+            <input id="text-input" class="relative block w-full appearance-none rounded-md rounded-r-none border
            border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500
             focus:z-10 focus:border-red-500 focus:outline-none
-             focus:ring-red-500 sm:text-sm" type="text" placeholder="Escribe un mensaje"/>
+             focus:ring-red-500 sm:text-sm" 
+             type="text" required placeholder="Escribe un mensaje"/>
              <button type="submit" class="group relative flex w-fit justify-center rounded-md rounded-l-none border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
           Enviar
         </button>
         </div>
         </form>
-        <div>
-            <ChatBubble :messages="messages_ordered"/>
-        </div>
     </div>
     </NuxtLayout>
 </template>
