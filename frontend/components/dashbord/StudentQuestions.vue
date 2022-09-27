83e42750c81984694cbe07775f9e040f8c7e031e
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-27 10:54:55 -0300

    add chat icon next to student questions

--- frontend/components/dashbord/StudentQuestions.vue
+++ frontend/components/dashbord/StudentQuestions.vue
@@ -25,9 +25,14 @@
         <h2 class="text-2xl font-semibold mb-4 mt-4">Mis dudas:</h2>
         <div class="space-y-3">
         <div v-for="question,i in questions">
-            <p><span class="font-semibold text-red-500">{{(i+1) + ". "}}</span> {{question.text}}</p>
+            <div class="flex space-x-4 items-center">
+            <p class="leading-8"><span class="font-semibold text-red-500">{{(i+1) + ". "}}</span> {{question.text}}</p>
+            <a :href="'/dashbord/chat/' + question.ID">
+                <img class="h-8 w-6 hover:scale-125" src="/chat.svg" alt="chat icon" />
+            </a>
+        </div>
             <div class="flex items-center">
-                <p class="text-red-500" v-if="question.response">{{"Respuesta: " + question.response}}</p>
+                <p v-if="question.response"> <span class="text-red-500">{{"Respuesta: "}}</span>{{question.response}}</p>
             <p v-else><span class="text-red-500 m-0">Respuesta:</span> -</p>
             </div>
         </div>
